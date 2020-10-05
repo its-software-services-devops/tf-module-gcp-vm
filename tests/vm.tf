4adb6d352baff6b99633bc5c9e4ba62b3dc68834
dmp-devops <seubpong.mon@truedigital.group>
2020-10-05 21:40:56 +0700

    Added gs bucket backend

--- tests/vm.tf
+++ tests/vm.tf
@@ -1,3 +1,16 @@
+# Run this at the beginning : export GOOGLE_CREDENTIALS="D:/dev/keys/its-artifact-commons-6eb8e8c315b3.json"
+
+terraform {
+  required_providers {
+    google = "~> 3.4.0"
+  }
+
+  backend "gcs" {
+    bucket  = "its-terraform-states"
+    prefix  = "tf-module-gcp-vm"
+  }  
+}
+
 provider "google" {  
   project     = "its-artifact-commons"
   region      = "asia-southeast1"
